d14bd6a8fc9234d9b231a241a86b35579b4e4421
Akintunde Adegbayo <Akt231@yahoo.com>
2023-10-23 19:32:42 +0100

    first proper commit

--- main.tf
+++ main.tf
@@ -7,18 +7,38 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" # define region as per your account
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "class54_bucket" {
-  bucket = "class54-s3-bucket"
+#terraform {
+#  backend "s3" {
+#    encrypt        = true
+#    bucket         = "class54-terraform-state"
+#    key            = "tfstate-s3-bucket"
+#    region         = "us-east-1"
+#    dynamodb_table = "terraform-state-lock-dynamo"
+#  }
+#}
 
-  object_lock_enabled = false
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "class54-terraform-state"
 
-  tags = {
-    Environment = "Prod"
+  versioning {
+    enabled = true
   }
 }
-#===============================
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
