3ad8975e00be4ee133988b52a4da63bb3ab2afd2
Akintunde Adegbayo <Akt231@yahoo.com>
2023-10-23 17:43:29 +0100

    main.tf changed

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1" # define region as per your account
 }
 
-resource "aws_s3_bucket" "class54 bucket" {
+resource "aws_s3_bucket" "class54_bucket" {
   bucket = "class54-s3-bucket"
 
   object_lock_enabled = false
